cc564442972bdb0c4a0ceabe7a45e4d8aa228585
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2025-05-05 00:44:58 -0500

    Refactor database and secret key configuration to use environment variables

--- 02-monolithic-scaling/config.py
+++ 02-monolithic-scaling/config.py
@@ -1,5 +1,14 @@
 import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
-SECRET_KEY = 'secretkey'
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER', 'bookstore_user')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'bookstore_pass')
+DB_HOST = os.getenv('DB_HOST', 'db')
+DB_NAME = os.getenv('DB_NAME', 'bookstore')
+DB_PORT = os.getenv('DB_PORT', '3306')
+
+SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'secretkey')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
